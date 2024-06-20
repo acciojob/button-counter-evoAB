ef1b7278b3415df6e1c9576651e9da7bb6b688bb
Abhishek <abhi@gmail.com>
2024-06-20 16:09:25 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React from "react";
 import './../styles/App.css';
-
+import {useState} from "react";
 const App = () => {
+  let [count, setCount] = useState(0)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p style={{fontFamily:"bold"}}>Button clicked {count} times</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Click me</button>
     </div>
   )
 }
